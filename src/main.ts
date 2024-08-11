ecf0102e5e48b221eb77711c009a08fbc149a563
¨joaquin <joakig6@gmail.com>
2024-08-10 22:12:35 -0300

    mend
    update folders to add front ens

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  console.log(`Listening on port ${process.env.PORT}`);
-  await app.listen(process.env.PORT);
-}
-bootstrap();
